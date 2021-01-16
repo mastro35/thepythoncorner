4923ad3f64d67945e084e77fb8e217b2d18662c6
Davide Mastromatteo <mastro35@gmail.com>
2021-01-16 16:18:08 +0100

    how to create a Python bot article

---
+++ examples/mastrobot_example.py
@@ -0,0 +1,46 @@
+# mastrobot.py
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# function to handle the /start command
+def start(update, context):
+    update.message.reply_text('start command received')
+
+# function to handle the /help command
+def help(update, context):
+    update.message.reply_text('help command received')
+
+# function to handle errors occured in the dispatcher 
+def error(update, context):
+    update.message.reply_text('an error occured')
+
+# function to handle normal text 
+def text(update, context):
+    text_received = update.message.text
+    update.message.reply_text(f'did you said "{text_received}" ?')
+
+def main():
+    TOKEN = "1432597051:AAHyX3CPg_gm5TNeneBHYbX6TqZrdkB7DHA"
+
+    # create the updater, that will automatically create also a dispatcher and a queue to 
+    # make them dialoge
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # add handlers for start and help commands
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+
+    # add an handler for normal text (not commands)
+    dispatcher.add_handler(MessageHandler(Filters.text, text))
+
+    # add an handler for errors
+    dispatcher.add_error_handler(error)
+
+    # start your shiny new bot
+    updater.start_polling()
+
+    # run the bot until Ctrl-C
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
