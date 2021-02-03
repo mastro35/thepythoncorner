8b0d6d6ef2998ee8b801332d2d90189fd159fcb8
mastro35 <mastro35@gmail.com>
2021-02-03 17:22:59 +0100

    Display cookie law

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,12 @@
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000",
+      "text": "#0f0"
+    },
+    "button": {
+      "background": "#0f0"
+    }
+  },
+  "type": "opt-in"
+});
