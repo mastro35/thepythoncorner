937387103da5b16643d21e0c324214e3dcb829fb
Davide Mastromatteo <mastro35@gmail.com>
2021-02-03 19:43:15 +0100

    opt in consent for google cookies

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -11,6 +11,38 @@ window.cookieconsent.initialise({
   "type": "opt-in",
   "content": {
     "href": "https://thepythoncorner.com/privacy/"
-  }
+  },
+  onInitialise: function (status) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onStatusChange: function (status, chosenBefore) {
+    var type = this.options.type;
+    var didConsent = this.hasConsented();
+    if (type == 'opt-in' && didConsent) {
+      // enable cookies
+      loadGAonConsent();
+    }
+    if (type == 'opt-out' && !didConsent) {
+      // disable cookies
+    }
+  },
+  onRevokeChoice: function () {
+    var type = this.options.type;
+    if (type == 'opt-in') {
+      // disable cookies
+    }
+    if (type == 'opt-out') {
+      // enable cookies
+      loadGAonConsent();
+    }
+  }  
 });
 
