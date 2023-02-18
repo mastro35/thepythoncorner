e75ce03b08ae6f9433178ed512a1bc2db04f2c3d
Davide Mastromatteo <mastro35@gmail.com>
2023-02-18 23:13:36 +0100

    restyling

--- examples/json1.py
+++
@@ -1,9 +0,0 @@
-# json1.py
-import json
-
-my_list = ["this","is","a","simple","list",35]
-my_json_object = json.dumps(my_list)
-print(my_json_object)
-
-my_second_list = json.loads(my_json_object)
-print(my_second_list)
